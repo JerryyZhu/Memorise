b159053302c1357f60fd850ae2137b2096c3cf1f
Jerry Zhu <jerryzhu96@hotmail.com>
2021-09-23 19:52:16 +1000

    initial

---
+++ Memorise/MemoriseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoriseApp.swift
+//  Memorise
+//
+//  Created by Jerry Zhu on 23/9/21.
+//
+
+import SwiftUI
+
+@main
+struct MemoriseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
