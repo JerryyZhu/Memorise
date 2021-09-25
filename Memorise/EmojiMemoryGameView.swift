a4fa971f5c2be8407c9ed598db24558d235948f5
Jerry Zhu <jerryzhu96@hotmail.com>
2021-09-25 23:27:24 +1000

    lecture 6: shapes

--- Memorise/EmojiMemoryGameView.swift
+++ Memorise/EmojiMemoryGameView.swift
@@ -37,6 +37,7 @@ struct CardView: View {
                 if card.isFaceUp {
                     shape.fill().foregroundColor(.white)
                     shape.strokeBorder(lineWidth: DrawingConstants.lineWidth)
+                    Pie(startAngle: Angle(degrees: 0-90), endAngel: Angle(degrees: 110-90)).padding(5).opacity(DrawingConstants.circleOpacity)
                     Text(card.content).font(font(in: geometry.size))
                 } else if card.isMatched{
                     shape.opacity(0)
@@ -54,7 +55,8 @@ struct CardView: View {
     private struct DrawingConstants {
         static let cornerRadius: CGFloat = 10
         static let lineWidth: CGFloat = 3
-        static let fontScale: CGFloat = 0.75
+        static let fontScale: CGFloat = 0.7
+        static let circleOpacity = 0.5
     }
 }
 struct ContentView_Previews: PreviewProvider {
--- Memorise/Pie.swift
+++ Memorise/Pie.swift
@@ -5,4 +5,35 @@
 //  Created by Jerry Zhu on 25/9/21.
 //
 
-import Foundation
+import SwiftUI
+
+struct Pie: Shape {
+    var startAngle: Angle
+    var endAngel: Angle
+    var clockwise = false
+    
+    func path(in rect: CGRect) -> Path {
+        
+        let center = CGPoint(x: rect.midX, y: rect.midY)
+        let radius = min(rect.width, rect.height) / 2
+        let start = CGPoint(
+            x: center.x + radius * CGFloat(cos(startAngle.radians)),
+            y: center.y + radius * CGFloat(sin(startAngle.radians))
+        )
+        
+        var path = Path()
+        path.move(to: center)
+        path.addLine(to: start)
+        path.addArc(
+            center: center,
+            radius: radius,
+            startAngle: startAngle,
+            endAngle: endAngel,
+            clockwise: !clockwise
+        )
+        path.addLine(to: center)
+        
+        return path
+    }
+    
+}
